daa3507f375b1fcd36026237636b8cf42319c9ae
nchen0 <nicky.chen0@gmail.com>
2019-05-20 20:53:42 -0400

    init

---
+++ tutorial/quickstart/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=20)
+    author = models.CharField(max_length=20)
+    year_released = models.IntegerField()
+    
